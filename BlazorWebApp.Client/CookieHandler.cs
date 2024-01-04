f37e25dcc3c8ce5d847b237468aaf030de1397c5
Aaron Prohaska <mohaaron@gmail.com>
2024-01-04 09:22:55 -0700

    Use CookieHandler for HttpClient

--- BlazorWebApp.Client/CookieHandler.cs
+++ BlazorWebApp.Client/CookieHandler.cs
@@ -4,36 +4,45 @@ using static System.Net.WebRequestMethods;
 using System.Net.Http.Headers;
 
 namespace BlazorWebApp.Client;
-
 public class CookieHandler : DelegatingHandler
 {
-    private readonly IAccessTokenProvider tokenProvider;
-
-    public CookieHandler(IAccessTokenProvider tokenProvider)
-    {
-        this.tokenProvider = tokenProvider;
-    }
-
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
-
-        var tokenResult = await tokenProvider.RequestAccessToken();
-
-        if (tokenResult.TryGetToken(out var token))
-        {
-            request.Headers.Authorization =
-                new AuthenticationHeaderValue("Bearer", token.Value);
-
-            //request.Content.Headers.TryAddWithoutValidation(
-            //    "x-custom-header", "value");
-
-            //var response = await Http.SendAsync(requestMessage);
-            //var responseStatusCode = response.StatusCode;
-
-            //responseBody = await response.Content.ReadAsStringAsync();
-        }
-
+        request.SetBrowserRequestMode(BrowserRequestMode.NoCors);
         return await base.SendAsync(request, cancellationToken);
     }
 }
+
+//public class CookieHandler : DelegatingHandler
+//{
+//    private readonly IAccessTokenProvider tokenProvider;
+
+//    public CookieHandler(IAccessTokenProvider tokenProvider)
+//    {
+//        this.tokenProvider = tokenProvider;
+//    }
+
+//    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+//    {
+//        request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
+
+//        var tokenResult = await tokenProvider.RequestAccessToken();
+
+//        if (tokenResult.TryGetToken(out var token))
+//        {
+//            request.Headers.Authorization =
+//                new AuthenticationHeaderValue("Bearer", token.Value);
+
+//            //request.Content.Headers.TryAddWithoutValidation(
+//            //    "x-custom-header", "value");
+
+//            //var response = await Http.SendAsync(requestMessage);
+//            //var responseStatusCode = response.StatusCode;
+
+//            //responseBody = await response.Content.ReadAsStringAsync();
+//        }
+
+//        return await base.SendAsync(request, cancellationToken);
+//    }
+//}
--- BlazorWebApp.Client/Program.cs
+++ BlazorWebApp.Client/Program.cs
@@ -11,11 +11,21 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
 builder.Services.AddTransient<CustomAuthorizationMessageHandler>();
+builder.Services.AddTransient<CookieHandler>();
 builder.Services.AddTransient<RemoteApiService>();
 
 builder.Services.AddHttpClient<RemoteApiService>(client =>
 {
     client.BaseAddress = new Uri("https://demo.duendesoftware.com/api/");
-}).AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+}).AddHttpMessageHandler<CookieHandler>();
+
+//builder.Services
+//    .AddTransient<CookieHandler>()
+//    .AddScoped(sp => sp
+//        .GetRequiredService<IHttpClientFactory>()
+//        .CreateClient("API"))
+//    .AddHttpClient("API", client => 
+//        client.BaseAddress = new Uri("https://demo.duendesoftware.com/api/"))
+//    .AddHttpMessageHandler<CookieHandler>();
 
 await builder.Build().RunAsync();
